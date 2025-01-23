f54e0365dc837d324703e94a88f60e57b941bd84
wcgregory <129503132+wcgregory@users.noreply.github.com>
2025-01-23 16:30:30 +0000

    single entry with list

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,35 @@ function App() {
 export default App
 */
 
-function App() {
+function SingleKsb(index=0) {
+  let ksb = ksbs[index]
+  return (
+    <li>
+      <p>Code: {ksb.Code}</p>
+      <p>Description: {ksb.Description}</p>
+    </li>
+  )
+}
+
+function ReturnKsbList() {
   const listKsbs = ksbs.map(ksb =>
-    <li> Code: {ksb.Code}</li>
+    <li>Code: {ksb.Code}</li>
   );
   return (
     <ul>{listKsbs}</ul>
+  )
+}
+
+function App() {
+  let ksb = ksbs[0]
+  return (
+    <div>
+      <h3>Single KSB</h3>
+        <p>{ksb.Code}</p>
+        <p>{ksb.Description}</p>
+      <h4>KSB List</h4>
+      <ReturnKsbList />
+    </div>
   )
 }
 
